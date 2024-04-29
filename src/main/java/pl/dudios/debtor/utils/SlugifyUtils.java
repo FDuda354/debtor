3869ea9b938037a72d4e44bb5b4e21ecba669633
Filip Duda <filip.duda9@icloud.com>
2024-04-29 23:04:02 +0200

    slugify

---
+++ src/main/java/pl/dudios/debtor/utils/SlugifyUtils.java
@@ -0,0 +1,24 @@
+package pl.dudios.debtor.utils;
+import com.github.slugify.Slugify;
+import org.apache.commons.io.FilenameUtils;
+
+public class SlugifyUtils {
+
+    private SlugifyUtils() {
+    }
+
+    public static String slugifyFileName(String fileName) {
+        String name = FilenameUtils.getBaseName(fileName);
+        Slugify slg = new Slugify();
+        String changedName = slg
+                .withCustomReplacement("_", "-")
+                .slugify(name);
+        return changedName + "." + FilenameUtils.getExtension(fileName);
+    }
+
+    public static String slugifySlug(String slug) {
+        return new Slugify()
+                .withCustomReplacement("_", "-")
+                .slugify(slug);
+    }
+}
